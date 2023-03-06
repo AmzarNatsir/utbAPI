116c0e7bab1370200beb7946571b2c08d547b4e0
amzar <amzarnatsir79@gmail.com>
2023-03-06 15:07:33 +0800

    Add Migration Unit, Supplier, Customer, Agent, Reseller

---
+++ database/migrations/2023_03_06_065216_create_customer.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer', function (Blueprint $table) {
+            $table->id();
+            $table->string("Name", 200);
+            $table->string("Address", 200);
+            $table->string("MobileNumber", 100);
+            $table->string("Email", 100);
+            $table->string("ContactPerson", 100);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer');
+    }
+};
